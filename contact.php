6ac4f9f7ff6a0a69ebba13af53bae84282720364
Rohit Tripathi <rohitink@live.com>
2013-04-07 21:19:24 +0530

    Added Missing Contact.php File.

---
+++ contact.php
@@ -0,0 +1,12 @@
+<?php 
+include('header.php');
+include('dbconnect.php');  ?>
+<div id="codes">
+<? 
+include('contactdetails.php');
+?>
+</div>
+<?
+include('sidebar.php');
+include('footer.php');
+?>
